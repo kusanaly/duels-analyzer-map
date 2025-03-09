10bb658ffc151678a93ee1d0aee44666e9ad9fbc
kusanaly <g.voscaniantz@gmail.com>
2025-03-09 19:52:59 +0000

    wo7

--- streamlit_app.py
+++ streamlit_app.py
@@ -652,14 +652,23 @@ class helpers:
             color_ = px.colors.sequential.Turbo
             metric_col = 'Your Distance'
         
-        
+        colorscale_ = [
+        [0, 'rgb(0, 0, 0)'], 
+        [0.4, 'rgb(243, 10, 10)'],
+        [0.6, 'rgb(234, 174, 19)'],
+        [0.8, 'rgb(220, 231, 22)'],
+        [0.9, 'rgb(26, 227, 40)'],
+        [0.96, 'rgb(23, 220, 210)'],
+        [0.998, 'rgb(17, 26, 179)'],
+        [1, 'rgb(238, 38, 218)']
+        ]
         fig = px.scatter_map(
             df,
             lat=lat_col,
             lon=lon_col,
             color=metric_col,
-            color_continuous_scale=color_,
-            color_continuous_midpoint=4500
+            color_continuous_scale=colorscale_,
+            zoom=0
         )
         if 'marker_size' not in st.session_state:
             st.session_state['marker_size'] = 4
