d4601623df5e38c943103323fb3610cafdd3eaa8
kusanaly <g.voscaniantz@gmail.com>
2025-03-11 00:02:58 +0000

    lmfao

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ class helpers:
                         data_dict['Your Longitude'].append(0)
                         data_dict['Your Distance'].append(0)
                         data_dict['Your Score'].append(0)
-
+                    sys.stdout.write(my_guess['score'])
                     if int(my_guess['score']) > 4950:
                         data_dict['5k Border'].append(11)
                     else:
