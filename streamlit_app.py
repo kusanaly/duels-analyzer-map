dc5cc80bf8fe43497883b29a01a344d0a57196a5
kusanaly <g.voscaniantz@gmail.com>
2025-03-15 14:19:09 +0000

    dammm

--- streamlit_app.py
+++ streamlit_app.py
@@ -1141,6 +1141,6 @@ if (submitted_token or st.session_state['submitted_token']) and _ncfa:
                         return clickData
 
                     if __name__ == '__main__':
-                        app.run_server(debug=True)
+                        app.run(debug=True)
                     #### Complete extracted data (Download for your own analysis)')
                     st.write(df_filtered)
