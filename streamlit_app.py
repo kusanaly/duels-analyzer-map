c9edcae469b1d720fb9af7b913722febef18f702
kusanaly <g.voscaniantz@gmail.com>
2025-03-16 00:06:11 +0000

    baby

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,95 @@ app.layout = [
             )
     ]
 
+lat_col = []
+lon_col = []
+df_filtered = dict({'Date': [],
+                         'Game Id': [],
+                          'Round Number': [],
+                          'Country': [],
+                          'Latitude': [],
+                          'Longitude': [],
+                          'Damage Multiplier': [],
+                          'Opponent Id': [],
+                          'Opponent Country': [],
+                          'Your Latitude': [],
+                          'Your Longitude': [],
+                          'Opponent Latitude': [],
+                          'Opponent Longitude': [],
+                          'Your Distance': [],
+                          'Opponent Distance': [],
+                          'Your Score': [],
+                          'Opponent Score': [],
+                          'Map Name': [],
+                          'Game Mode': [],
+                          'Moving': [],
+                          'Zooming': [],
+                          'Rotating': [],
+                          'Your Rating': [],
+                          'Opponent Rating': [],
+                          'Score Difference': [],
+                          'Win Percentage': [],
+                          '5k Border': [],
+                          'Pano URL': []
+                          })
+metric_col = "Your Distance"
+
+color_ = {"sequential": [
+                    [0, 'rgb(191, 34, 34)'], 
+                    [0.3, 'rgb(243, 10, 10)'],
+                    [0.5, 'rgb(234, 174, 19)'],
+                    [0.75, 'rgb(220, 231, 22)'],
+                    [0.85, 'rgb(26, 227, 40)'],
+                    [0.90, 'rgb(34, 187, 175)'],
+                    [0.95, 'rgb(24, 111, 197)'],
+                    [0.995, 'rgb(47, 47, 255)'],
+                    [0.996, 'rgb(255, 255, 255)'],
+                    [1, 'rgb(255, 255, 255)']
+                    ]}
+
+if metric_col == 'Distance':
+    metric_col = 'Your Distance'
+if metric_col == 'Your Score':
+    color_ = {"sequential": [
+    [0, 'rgb(191, 34, 34)'], 
+    [0.3, 'rgb(243, 10, 10)'],
+    [0.5, 'rgb(234, 174, 19)'],
+    [0.75, 'rgb(220, 231, 22)'],
+    [0.85, 'rgb(26, 227, 40)'],
+    [0.90, 'rgb(34, 187, 175)'],
+    [0.95, 'rgb(24, 111, 197)'],
+    [0.995, 'rgb(47, 47, 255)'],
+    [0.996, 'rgb(255, 255, 255)'],
+    [1, 'rgb(255, 255, 255)']
+    ]}
+if metric_col == 'Your Distance':
+    color_ = {"sequential": [
+            [0, 'rgb(255, 255, 255)'], 
+            [0.00025, 'rgb(255, 255, 255)'],
+            [0.000255, 'rgb(85, 9, 213)'],
+            [0.0025, 'rgb(35, 31, 119)'],
+            [0.005, 'rgb(20, 102, 212)'],
+            [0.025, 'rgb(17, 155, 166)'],
+            [0.05, 'rgb(24, 111, 197)'],
+            [0.1, 'rgb(47, 47, 255)'],
+            [0.25, 'rgb(255, 255, 255)'],
+            [1, 'rgb(255, 255, 255)']
+            ]}
+if metric_col == 'Score Difference':
+    color_ = {"sequential": [
+            [0, 'rgb(153, 26, 29)'], 
+            [0.2, 'rgb(224, 69, 10)'],
+            [0.4, 'rgb(222, 97, 65)'],
+            [0.45, 'rgb(224, 183, 152)'],
+            [0.475, 'rgb(211, 188, 165)'],
+            [0.5, 'rgb(221, 221, 221)'],
+            [0.525, 'rgb(193, 222, 192)'],
+            [0.55, 'rgb(128, 213, 128)'],
+            [0.6, 'rgb(68, 213, 86)'],
+            [0.8, 'rgb(7, 156, 89)'],
+            [1, 'rgb(7, 41, 156)']
+            ]}
+
 fig.add_trace(go.Scattermap(
         lat=lat_col,
         lon=lon_col,
@@ -103,7 +192,7 @@ def update_scatter_plot(clickData):
 def display_click_data(clickData):
     if clickData:
         point_index = clickData['points'][0]['pointIndex']
-    url = df['Pano URL'][point_index]
+    url = df_filtered['Pano URL'][point_index]
     # Open the URL in a new tab
     import webbrowser
     webbrowser.open(url)
